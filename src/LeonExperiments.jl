22de51ab1007d278f90e047fd91178a71f2c8f59
Leon Riley <114745825+Leonril@users.noreply.github.com>
2025-11-15 17:27:45 +0000

    Initial commit

---
+++ src/LeonExperiments.jl
@@ -0,0 +1,10 @@
+module LeonExperiments
+
+using Comodo
+
+function demo()
+    println("Running MyComodoProject with Comodo!")
+    # Example: create a simple 3D mesh or visualization later
+end
+
+end # module
