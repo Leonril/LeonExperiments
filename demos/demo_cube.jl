45c021693b58213748cae9b332a16917c684fd38
Leon Riley <114745825+Leonril@users.noreply.github.com>
2025-11-27 17:09:44 +0000

    Added demo to create a cube and plot it to a figure

---
+++ demos/demo_cube.jl
@@ -0,0 +1,11 @@
+# demo_cube.jl
+# Description: A demo script to create and display a cube using Comodo.
+
+#using LeonExperiments
+using GLMakie
+
+F,V = Comodo.cube(7)
+fig = Figure()
+ax=Axis3(fig[1,1])
+Comodo.meshplot!(ax, F, V)
+display(fig)
