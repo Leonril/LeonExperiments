db8f826d36e0d64c8af430e4b9386ca81ca2e073
Leon Riley <114745825+Leonril@users.noreply.github.com>
2025-12-11 19:07:10 +0000

    Finished batman demo, sides and stl generation working correctly

--- demos/demo_stl.jl
+++ demos/demo_stl.jl
@@ -48,71 +48,73 @@ if testCase == 1
     save(File_name, surfaceMesh); #Save stl
 
 elseif testCase == 2
+    # Generating Batman curve
     Vb=batman(50) #List of nodes of the Batman logo
-    @show length(Vb)  # currently showing 323
 
     #scaling curve
     width = 40.0 # desired width of batman logo in mm
     Vb = Vb .* (width / 2) # Scaling, default width is 2 mm, so dividing by 2
-    @show length(Vb)  # currently showing 323
 
     # Visualling Vb Batman curve
     fig = Figure(size=(1000,500))
-    ax1 = AxisGeom(fig[1, 1]; title="stepwise=true, approximately n points, anti-clockwise", azimuth=-pi/2, elevation=pi/2)
+    ax1 = AxisGeom(fig[1, 1]; title="Boundary Vertices", azimuth=-pi/2, elevation=pi/2)
     hp1 = lines!(ax1, Vb,linewidth=3,color=:blue)
     hp2 = scatter!(ax1, Vb,markersize=8,color=:red)
-    hp2 = scatter!(ax1, Vb[1],markersize=15,color=:yellow)
-    hp2 = scatter!(ax1, Vb[2],markersize=15,color=:orange)
-    display(fig)
+    screen = display(GLMakie.Screen(), fig)
 
     # Setting up regiontrimesh inputs
-    @show length(Vb)  # currently showing 323
-
-    VT = (copy(Vb),) # Tuple of the vertex array
-    @show length(Vb)  # currently showing 323
-
+    VT = (copy(Vb),) # Tuple of the vertex array, regiontrimesh seems to mutate Vb so use a copy
     R = ([1],) # Tuple of the region to be meshed
-    pointSpacing = pointspacingmean(copy(Vb); close_loop=true) # Average spacing of the points on the curve
-    P = (pointSpacing)
+    P = pointspacingmean(copy(Vb); close_loop=true) # Average spacing of the points on the curve
     
     # Creating triangular mesh of batman logo
-    F,V,C = regiontrimesh(VT,R,P)
- 
+    F,Vbottom,C = regiontrimesh(VT,R,P)
    
     # Plotting triangular mesh
-    fig2 = Figure(size=(1200,1000))
-    ax2 = AxisGeom(fig2[1, 1], title="Triangular mesh", azimuth=-pi/2, elevation=pi/2)
-    hp1 = meshplot!(ax2, F, V, strokewidth=1.0, color=:lightblue)    
-
-    screen = display(GLMakie.Screen(), fig2)
-    GLMakie.set_title!(screen, "Tri mesh of Batman Logo")
+    fig = Figure(size=(1200,1000))
+    ax1 = AxisGeom(fig2[1, 1], title="Triangular mesh", azimuth=-pi/2, elevation=pi/2)
+    hp1 = meshplot!(ax1, F, Vbottom, strokewidth=1.0, color=:lightblue)    
+    screen = display(GLMakie.Screen(), fig)
 
     # Copying mesh for top of extrude
     height = 5.0
-    Vtop = [Point(v[1], v[2], v[3] + height) for v in V]
+    Vtop = [Point(v[1], v[2], v[3] + height) for v in Vbottom]
     Ctop = C .+ 1 # Shift color indices for top faces
 
     #Plotting top and bottom meshes
     fig = Figure(size=(1200,1000))
     ax1 = AxisGeom(fig[1, 1], title="Top and bottom faces", azimuth=-pi/2, elevation=pi/2)
-    hp1 = meshplot!(ax1, F, V, strokewidth=1.0, color=:red)
+    hp1 = meshplot!(ax1, F, Vbottom, strokewidth=1.0, color=:red)
     hp1 = meshplot!(ax1, F, Vtop, strokewidth=1.0, color=:lightblue)
     screen = display(GLMakie.Screen(), fig)
 
     # shift Ftop
-    offset = length(V) # number of vertices in bottom face
+    offset = length(Vbottom) # number of vertices in bottom face
     Ftop = [ # shifting indices of top face triangles
         TriangleFace(f[1] + offset, f[2] + offset, f[3] + offset)
     for f in F]
 
+    # Finding the boundary edges for extrude sides
+    indexmap = Dict(Vbottom[i] => i for i in eachindex(Vbottom))
+
+    Vb_bottomInd = [ get(indexmap, p, nothing) for p in Vb ] # Find indexes of boundary points Vb in Vbottom
+    Vb_bottomInd = filter(!isnothing, Vb_bottomInd) # Remove any missing points
+
+    Vb_topInd = [i + offset for i in Vb_bottomInd] #Indexes of boundary points for top of the curve 
+    
+
     # Creating side faces of extrude
     n = length(Vb) # number of vertices in bottom face
-    Fs1 = [TriangleFace(i, i+1, offset+i) for i in 1:(n-2)] # one half of quad split into two tris
-    Fs2 = [TriangleFace(i+offset, i+1+offset, i+1) for i in 1:(n-2)] # other half of quad split into two tris
-    C_sides = fill(maximum(C) + 1, length(Fs1) + length(Fs2)) # New color for side faces
+    Fs1 = [TriangleFace(Vb_bottomInd[i], Vb_bottomInd[i+1], Vb_topInd[i]) for i in 1:(n-1)] # one half of quad split into two tris
+    Fs2 = [TriangleFace(Vb_topInd[i], Vb_topInd[i+1], Vb_bottomInd[i+1]) for i in 1:(n-1)] # other half of quad split into two tris
+    
+    push!(Fs1, TriangleFace(Vb_bottomInd[n], Vb_bottomInd[1], Vb_topInd[n])) # closing the loop of faces
+    push!(Fs2, TriangleFace(Vb_topInd[n], Vb_topInd[1], Vb_bottomInd[1]))
+    
+    C_sides = fill(maximum(Ctop)+1, length(Fs1) + length(Fs2)) # New color for side faces
 
     # combine geometry
-    V = [V; Vtop]
+    V = [Vbottom; Vtop]
     Fb = [F; Ftop; Fs1; Fs2]
     Cb = [C; Ctop; C_sides]
 
@@ -126,10 +128,14 @@ elseif testCase == 2
     Colorbar(fig4[1, 1][1, 2], hp4)
     screen = display(GLMakie.Screen(), fig4)
 
-    # Use triangular mesh to creat tetrahedral mesh
-    # or use it to create a quadrahedral mesh and then convert those to tris
-    #convert to stl
-    #save stl
+    #convert to triangular mesh
+    surfaceMesh=GeometryBasics.Mesh(Point.(V),Fb)
+
+    #Save as .stl
+    export_dir = joinpath(@__DIR__, "..", "exports") #Adds exports folder in parent directory
+    isdir(export_dir) || mkdir(export_dir) #Create exports folder if it doesn't exist
+    File_name = joinpath(export_dir,"demo_stl_batman.stl") #Define file path and name
+    save(File_name, surfaceMesh); #Save stl
 
 elseif testCase == 3
     # Creating holly berries 
