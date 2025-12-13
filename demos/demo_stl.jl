f666f587eb1f57037b3c5d0e3842705e041888d4
Leon Riley <114745825+Leonril@users.noreply.github.com>
2025-12-13 18:37:54 +0000

    Progress on the base of the ornament, have the outer Vb generating correctly.

--- demos/demo_stl.jl
+++ demos/demo_stl.jl
@@ -19,10 +19,11 @@ using GeometryBasics
 using MeshIO
 using FileIO
 using FilePathsBase
+using LinearAlgebra
 
 GLMakie.closeall() #Close any open Makie windows
 
-testCase = 2# Change this value to switch between test cases
+testCase = 3 # Change this value to switch between test cases
 #testCase = 1: Create and save an stl of a cube
 #testCase = 2: Create and save an stl of an extruded batman logo from Comodo
 #testCase = 3: Create and save an stl of a Christmas ornament using an overengineered levelset method, for practice
@@ -89,20 +90,17 @@ elseif testCase == 2
     screen = display(GLMakie.Screen(), fig)
 
     # shift Ftop
-    offset = length(Vbottom) # number of vertices in bottom face
+    offset = length(Vbottom) # number of vertices in bottom tri mesh faces
     Ftop = [ # shifting indices of top face triangles
         TriangleFace(f[1] + offset, f[2] + offset, f[3] + offset)
     for f in F]
 
-    # Finding the boundary edges for extrude sides
+    # Finding the boundary edges for extrude sides, regiontrimesh does not return boundary vertex order
     indexmap = Dict(Vbottom[i] => i for i in eachindex(Vbottom))
-
     Vb_bottomInd = [ get(indexmap, p, nothing) for p in Vb ] # Find indexes of boundary points Vb in Vbottom
     Vb_bottomInd = filter(!isnothing, Vb_bottomInd) # Remove any missing points
-
     Vb_topInd = [i + offset for i in Vb_bottomInd] #Indexes of boundary points for top of the curve 
     
-
     # Creating side faces of extrude
     n = length(Vb) # number of vertices in bottom face
     Fs1 = [TriangleFace(Vb_bottomInd[i], Vb_bottomInd[i+1], Vb_topInd[i]) for i in 1:(n-1)] # one half of quad split into two tris
@@ -139,21 +137,160 @@ elseif testCase == 2
 
 elseif testCase == 3
     # Creating holly berries 
-    FBerryBase, VBerryBase = Comodo.geosphere(3,10) # Creates a sphere mesh
+    rBerry = 5.0
+    pointSpacingBerry = 1.0
+    z_threshold = -(rBerry*0.85) # Height at which berry meets base
+    FBerry, VBerry = geosphere(2,rBerry) # Creates a sphere mesh
+
+    # Plotting berry tri sphere
+    fig = Figure(size=(1000,500))
+    ax=Axis3(fig[1, 1], title="Berry (tri sphere)", azimuth=-pi/2, elevation=pi/2)
+    Comodo.meshplot!(ax, FBerry, VBerry)
+    dscreen = display(GLMakie.Screen(), fig)
+
+    # Remove lower part of the berry to intergrate with base
+    idx = findall(getindex.(VBerry, 3) .< z_threshold) # Find indices of verticecs below z_threshold
+    setRemove = Set(idx) # Converting to a set  
+    F_keep = [f for f in FBerry if all(v -> !(v in setRemove), f)] # Using set to remove faces with vertices below threshold
+
+    # Plotting berry with bottom removed
+    fig = Figure(size=(1000,500))
+    ax=Axis3(fig[1, 1], title="Berry jagged cut ", azimuth=-pi/2, elevation=pi/2)
+    Comodo.meshplot!(ax, F_keep, VBerry)
+    dscreen = display(GLMakie.Screen(), fig)
+
+    # Even out z height of bottom of berry edges
+    F_keep_cleaned, Vkeep  = remove_unused_vertices(F_keep, VBerry) # Remove unused vertices after face removal
+    FaceConnectivity = con_vertex_face(F_keep_cleaned, Vkeep)# List of how many faces touch each vertex
+
+    # Suppose connectivityV[i] is a vector of connected vertices for vertex i
+    edge_indices = [i for i in 1:length(test) if length(FaceConnectivity[i]) < 5] #Edges have less than 5 connected faces
+
+    for i in edge_indices # Update z value of edge vertices from cut faces
+    v = Vkeep[i]
+    Vkeep[i] = Point(v[1], v[2], z_threshold)  # create a new Point with updated z
+    end
+
+    Vedge = Vkeep[edge_indices] # Store edge vertices 
+
+    # Plotting berry with cleaned bottom edges
+    fig = Figure(size=(1000,500))
+    ax=AxisGeom(fig[1, 1], title="Berry  with cleaned cut ", azimuth=-pi/2, elevation=pi/2)
+    hp1 = Comodo.meshplot!(ax, F_keep_cleaned, Vkeep, color=:red)
+    hp2 = scatter!(ax, Vedge ,markersize=10,color=:blue)
+    dscreen = display(GLMakie.Screen(), fig)
+
+    # Creating the leaf boundary
+    rLeaf1 = 8.0 # radius of the curves of the leaves
+    rLeaf2 = 5.0 # radius of the smaller curves of the leaves
+    nLeafPoints = 40 # refinement control of the mesh, use multiple of 4 for symmetry
+    cut = rLeaf2/5  # cut to make smaller curve fit regularly 
+    errror = 0.01  # small error tolerance for point inclusion checks
+
+    V_leaf_circle1 = circlepoints(rLeaf1,nLeafPoints)
+    V_leaf_circle2 = circlepoints(rLeaf2,nLeafPoints)
+    
+    Varc1_unsorted = [v for v in V_leaf_circle1 if v[1] > 0-error && v[2] < 0+error]
+    Varc1 = [Varc1_unsorted[2:end]; Varc1_unsorted[1:1]] # Sorting points to go counter clockwise
+    Varc2 = [v for v in V_leaf_circle2 if v[2] > 0+cut] # Cutting the points for a curve from a circle
+
+    # Plotting leaf arcs
+    fig = Figure(size=(1000,500))
+    ax=AxisGeom(fig[1, 1], title="Leaf arcs ", azimuth=-pi/2, elevation=pi/2)
+    hp1 = scatter!(ax, Varc1 ,markersize=10,color=:blue)
+    hp2 = scatter!(ax, Varc2 ,markersize=10,color=:red)
+    dscreen = display(GLMakie.Screen(), fig)
+
+    # Using the arcs to create the leaf shape
+    VLeafCurve2 = reverse([Point(v[1] + (rLeaf1+rLeaf2), -v[2], v[3]) for v in Varc2])
+    VLeafCurve3 = reverse([Point(-v[1] + 2*(rLeaf1+rLeaf2), v[2], v[3]) for v in Varc1])
+    
+    VLeafHalf_unshifted = [Varc1; VLeafCurve2; VLeafCurve3] # Combine arcs to make half leaf
+    VLeafHalf1 = [Point(v[1], v[2]+rLeaf1, v[3]) for v in VLeafHalf_unshifted] # Shift leaf up to center around origin
+    VLeafHalf2 = reverse([Point(v[1], -v[2], v[3]) for v in VLeafHalf1])
+    VLeaf = [VLeafHalf1; VLeafHalf2[2:end-1]] # Complete leaf by mirroring half and trimming start and stop
+
+    # Plotting leaf arcs
+    fig = Figure(size=(1000,500))
+    ax=AxisGeom(fig[1, 1], title="Leaf arcs ", azimuth=-pi/2, elevation=pi/2)
+    hp1 = scatter!(ax, Varc1 ,markersize=10,color=:blue)
+    hp2 = scatter!(ax, VLeafCurve2 ,markersize=10,color=:red)
+    hp3 = scatter!(ax, VLeafCurve3 ,markersize=10,color=:purple)
+    dscreen = display(GLMakie.Screen(), fig)
 
     fig = Figure(size=(1000,500))
-    ax=Axis3(fig[1,1])
-    Comodo.meshplot!(ax, FBerryBase, VBerryBase)
-    display(fig)
+    ax=AxisGeom(fig[1, 1], title="Leaf V", azimuth=-pi/2, elevation=pi/2)
+    hp1 = scatter!(ax, VLeaf ,markersize=10,color=:darkgreen)
+    dscreen = display(GLMakie.Screen(), fig)
 
-    # Creating the leave levelsets
-
+    # Creating the base boundary
+    spacingBerry = 2.0 # spacing between berries
+    dist_berryCentres = 2*rBerry + spacingBerry # distance between berry centers
     
-    
-    #generate holly ornament levelset
-    #convert to triangular mesh
-    #plot holly ornament
-    #convert to stl
-    #save stl
+    # Radius of the circular base to fit three berries
+    radiusBerryCentre= ceil((sqrt(3)/3) * dist_berryCentres)
 
+    # first vertex at negative x-axis
+    centreBerry1 = [-radiusBerryCentre, 0.0]
+
+    # rotation matrix function
+        function rotate(v, θ)
+        [cos(θ) -sin(θ);
+         sin(θ)  cos(θ)] * v
+    end
+
+    θ = 2π/3
+    centreBerry2 = rotate(centreBerry1, θ)
+    centreBerry3 = rotate(centreBerry1, 2θ)
+
+    radiusBase = radiusBerryCentre + rBerry + spacingBerry 
+
+    # Trimming leaf and base to fit together
+    VLeafShifted = [Point(v[1]+0.8*radiusBase, v[2], v[3]) for v in VLeaf] # Shift leaf down to original position
+    VLeafTrimmed = [v for v in VLeafShifted if norm(v) > radiusBase]
+
+    fig = Figure(size=(1000,500))
+    ax=AxisGeom(fig[1, 1], title="Leaf V, positioned and trimmed for base", azimuth=-pi/2, elevation=pi/2)
+    hp1 = scatter!(ax, VLeafTrimmed ,markersize=10,color=:darkgreen)
+    dscreen = display(GLMakie.Screen(), fig)
+
+    #Creating base arc points
+    VLeadEnd = VLeafTrimmed[end]
+    angleLeaf = atan.(VLeadEnd[2], VLeadEnd[1])-0.02 # small offset to ensure no overlap
+    VBaseArc = reverse([Point(cos(θ)*radiusBase, sin(θ)*radiusBase, 0.0) for θ in ((2*(2π/3))-angleLeaf):pi/30:2π+angleLeaf])
+
+    fig = Figure(size=(1000,500))
+    ax=AxisGeom(fig[1, 1], title="Leaf and Base before rotation", azimuth=-pi/2, elevation=pi/2)
+    hp1 = scatter!(ax, VLeafTrimmed ,markersize=10,color=:darkgreen)
+    hp2 = scatter!(ax, VBaseArc ,markersize=10,color=:red)
+    dscreen = display(GLMakie.Screen(), fig)
+
+    # Using rotation to close the base shape
+    VBase1 = [VLeafTrimmed;VBaseArc]
+    
+    R2 = [cos(θ) -sin(θ); # Rotation matrix 
+     sin(θ)  cos(θ)]
+
+    # Rotate all points
+    VBase2 = [Point(R * [p[1]; p[2]]..., p[3]) for p in VBase1]
+    VBase3 = [Point(R * [p[1]; p[2]]..., p[3]) for p in VBase2]
+
+    # Combine all base points
+    Vb_Base = [VBase1; VBase2; VBase3] # Complete base points
+
+    fig = Figure(size=(1000,500))
+    ax=AxisGeom(fig[1, 1], title="Base Boundary Points", azimuth=-pi/2, elevation=pi/2)
+    hp1 = scatter!(ax, Vb_Base ,markersize=10,color=:darkgreen)
+    dscreen = display(GLMakie.Screen(), fig)
+
+   # To do from here:
+   # - position and track berries
+   # - Put hole in base for a string and track
+   # - copy base to negative thickness 
+   # - region tri mesh top and bottom
+   # - refind boundary edges 
+   # - create side faces
+   # - combine all
+   # - convert to stl
+ 
 end
